d0c14947b47a55f0ee4eebdb459a674a05e878f0
Kevin <kevin_sr_13@hotmail.com>
2021-03-11 16:18:50 +0100

    archivos html subidos

---
+++ HTML/UF2_AC1/uytransfer/index.php
@@ -0,0 +1,44 @@
+<!doctype html>
+<html lang="es">
+	<head>
+		<meta charset="utf-8">
+		<title>Uy!Transfer</title>
+		<link rel="stylesheet" type="text/css" href="css/estilos.css" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	</head>
+
+	<body>
+
+		<div class="container mt-5" id="contenedor">
+			<div class="row" id="row-titulo">
+				
+				<div class="col-6 offset-3 p-3 bg-success d-flex-column rounded-top" id="div-titulo">
+					
+					
+					<h1 class="text-white text-left" id="titulo">Uy!Transfer</h1>
+
+				</div>
+			
+			</div>
+
+			<div class="row" id="row-formulario">
+
+			<div class="col-6 offset-3 p-3 bg-success rounded-bottom" id="div-formulario">
+				
+					<form id="formulario" name="formulario" action="login.php" method="post">
+						<div class="form-group" id="form-group-usuario">
+							<input type="text" class="form-control form-control-lg text-primary mx-auto my-3 w-75" id="nombre" name="nombre" placeholder="Tu nombre">
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
+
+		
+		
+
+		<?php
+
+		?>
+	</body>
+</html>
